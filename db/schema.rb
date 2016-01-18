3f88190198232985663d193ef179842f7d8165af
vinney cavallo <vcavallo@gmail.com>
2016-01-18 00:26:03 -0500

    Add acts_as_list gem and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117210716) do
+ActiveRecord::Schema.define(version: 20160117223547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160117210716) do
     t.datetime "updated_at", null: false
     t.integer  "flavor_id"
     t.integer  "list_id"
+    t.integer  "position"
   end
 
   add_index "targets", ["flavor_id"], name: "index_targets_on_flavor_id", using: :btree
